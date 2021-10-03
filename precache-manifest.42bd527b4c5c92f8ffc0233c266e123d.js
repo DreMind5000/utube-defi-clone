b93cb9a8491feabb94c144faa62925595f710c49
DreMind5000 <89564302+DreMind5000@users.noreply.github.com>
2021-10-03 06:40:39 -0400

    Updates

---
+++ precache-manifest.42bd527b4c5c92f8ffc0233c266e123d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b6d115cce38bacdb189a7fb750f9e1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8aaf1591b56ce57d5f88",
+    "url": "./static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "11f8267da12cc3012eb8",
+    "url": "./static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "8aaf1591b56ce57d5f88",
+    "url": "./static/js/2.dd4807a3.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "./static/js/2.dd4807a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f8267da12cc3012eb8",
+    "url": "./static/js/main.647be2f5.chunk.js"
+  },
+  {
+    "revision": "fc71048510e0974eab3b",
+    "url": "./static/js/runtime-main.be13530a.js"
+  },
+  {
+    "revision": "9e02a88e54fb14899a2ea3c60bf8fa35",
+    "url": "./static/media/dvideo.9e02a88e.png"
+  }
+]);
